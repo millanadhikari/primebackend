3fb3755fbd83444259d3ca0246d0b7fd9a613e5c
millanadhikari <millanadhikari@gmail.com>
2025-07-19 11:44:23 +1000

    Fix filename casing from ClientController.js to clientController.js

--- src/routes/clientRoutes.js
+++ src/routes/clientRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { ClientController } from '../controllers/clientController.js';
+import {ClientController} from '../controllers/clientController.js'
 import { authenticateToken, requireAdmin, requireCoordinatorOrAdmin } from '../middleware/auth.js';
 
 const router = express.Router()
@@ -7,25 +7,25 @@ const router = express.Router()
 
 // AUTH ROUTES
 //Create a new client (authentication required)
-router.post('/signup', 
+router.post('/signup',
     // authenticateToken, requireCoordinatorOrAdmin, 
     ClientController.signup);
 
 // CLIENT ROUTES
 // Get all clients (authentication required)')
-router.get('/', 
+router.get('/',
     // authenticateToken, requireCoordinatorOrAdmin, 
     ClientController.getAllClients);
 //Get client by ID (authenticaFion required)
-router.get('/:id', 
+router.get('/:id',
     // authenticateToken, requireCoordinatorOrAdmin, 
     ClientController.getClientById);
 // Update client profile (authentication required)
-router.put('/:id', 
+router.put('/:id',
     // authenticateToken, requireCoordinatorOrAdmin, 
     ClientController.updateProfile);
 // Delete client profile (authentication required)
-router.delete('/:id', 
+router.delete('/:id',
     // authenticateToken, requireCoordinatorOrAdmin, 
     ClientController.deleteClientById);
 
