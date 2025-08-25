a3b1c1b66792c78cc7b0042a70cb6ad3ca36c3f6
millanadhikari <millanadhikari@gmail.com>
2025-08-25 21:34:10 +1000

    open ai error fixed2

--- src/config/openaiClient.mjs
+++ src/config/openaiClient.mjs
@@ -1,9 +1,9 @@
-import OpenAI from "openai";
+// import OpenAI from "openai";
 
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+// const openai = new OpenAI({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
 
 
 
-export default openai;
+// export default openai;
